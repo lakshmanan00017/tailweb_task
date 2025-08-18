88e0336f109da19d5b1a0e4e8b8eed0d036de8d4
Lakshmanan N <lakshmanancse000@gmail.com>
2025-08-18 14:39:06 +0530

    Update session.py

--- task_app/session.py
+++ task_app/session.py
@@ -2,29 +2,29 @@ import os
 import time
 
 
-SESSION_STORE = {}
-SESSION_DURATION = 30 * 60 
+session_store = {}
+session_duration = 30 * 60 
 
 def generate_token():
     return os.urandom(24).hex()
 
 def create_session(teacher_id):
     token = generate_token()
-    expiry = time.time() + SESSION_DURATION
-    SESSION_STORE[token] = {"teacher_id": teacher_id, "expiry": expiry}
+    expiry = time.time() + session_duration
+    session_store[token] = {"teacher_id": teacher_id, "expiry": expiry}
     return token
 
 def validate_session(token):
-    session = SESSION_STORE.get(token)
+    session = session_store.get(token)
     if not session:
         return None  
 
     if time.time() > session["expiry"]:
-        del SESSION_STORE[token]  
+        del session_store[token]  
         return None
 
     return session["teacher_id"]
 
 def destroy_session(token):
-    if token in SESSION_STORE:
-        del SESSION_STORE[token]
+    if token in session_store:
+        del session_store[token]
